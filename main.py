0f80bcce3b612e274764c43ec70bf791589cfae3
Ivan Wong <127964300+HDGraphic@users.noreply.github.com>
2023-03-17 00:21:51 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-# Importing the required libraries
-import time
-
 # Displaying the instructions
 print("Welcome to the depression detection test")
 print('Please answer the following questions using a scale of 0 to 3, where 0 = Never, 1 = Several days, 2 = half the days or more, 3 = Almost every day.\n")
