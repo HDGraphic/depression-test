ed063f6ccebef07bd205c77ff78fccac71e24c2a
Ivan Wong <127964300+HDGraphic@users.noreply.github.com>
2023-03-17 00:01:54 +0800

    Create results.py

---
+++ results.py
@@ -0,0 +1,10 @@
+# Displaying the results
+print(f"\nYour total score is {score}.")
+if score < 5:
+    print("You have minimal symptoms of depression.")
+elif score < 10:
+    print("You have mild symptoms of depression.")
+elif score < 15:
+    print("You have moderate symptoms of depression.")
+else:
+    print("You have severe symptoms of depression.") 
