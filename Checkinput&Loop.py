42912cb9465216ccaccddc95b829000663b4e33b
Ivan Wong <127964300+HDGraphic@users.noreply.github.com>
2023-03-17 00:38:11 +0800

    Update Checkinput&Loop.py

--- Checkinput&Loop.py
+++ Checkinput&Loop.py
@@ -2,6 +2,8 @@
 for question in questions:
     print(question)
     answer = input("Enter your answer (0-3): ")
-    while not answer.isdigit() or int(answer) not in [0, 1, 2, 3]:
-        answer = input("Invalid answer. Please enter a number from 0 to 3: ")
-    answers.append(int(answer))
+    if not answer.isdigit() or int(answer) not in [0, 1, 2, 3]:
+        print("Invalid answer. The program will now terminate.")
+        exit()
+    else:
+        answers.append(int(answer))
