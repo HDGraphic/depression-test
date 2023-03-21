7027b2fa8783aa349bb6bf72986b932a10c29e19
Ivan Wong <127964300+HDGraphic@users.noreply.github.com>
2023-03-21 18:02:00 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,15 +5,15 @@ def introduction():
       print('Please answer the following questions using a scale of 0 to 3, where:\n0 = Never, 1 = Several days, 2 = half the days or more, 3 = Almost every day.\n')
 
 def questions():
-    questions = ["Little interest or pleasure in doing things?", 
-             "Feeling down, depressed, or hopeless?", 
-             "Trouble falling or staying asleep, or sleeping too much?", 
-             "Feeling tired or having little energy?", 
-             "Poor appetite or overeating?", 
-             "Feeling bad about yourself - or that you are a failure or have let yourself or your family down?", 
-             "Trouble concentrating on things, such as reading the newspaper or watching television?", 
-             "Moving or speaking so slowly that other people could have noticed. Or the opposite - being so fidgety or restless that you have been moving around a lot more than usual?", 
-             "Thoughts that you would be better off dead or of hurting yourself in some way?"]
+    questions = ["1) Little interest or pleasure in doing things?", 
+             "2) Feeling down, depressed, or hopeless?", 
+             "3) Trouble falling or staying asleep, or sleeping too much?", 
+             "4) Feeling tired or having little energy?", 
+             "5) Poor appetite or overeating?", 
+             "6) Feeling bad about yourself - or that you are a failure or have let yourself or your family down?", 
+             "7) Trouble concentrating on things, such as reading the newspaper or watching television?", 
+             "8) Moving or speaking so slowly that other people could have noticed. Or the opposite - being so fidgety or restless that you have been moving around a lot more than usual?", 
+             "9) Thoughts that you would be better off dead or of hurting yourself in some way?"]
     
     score = 0
     answers = []
