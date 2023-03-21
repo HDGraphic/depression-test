fa0b5707641fbdd20c026654ed3d9c41095bec73
Ivan Wong <127964300+HDGraphic@users.noreply.github.com>
2023-03-21 18:09:18 +0800

    Update main.py

--- main.py
+++ main.py
@@ -46,7 +46,9 @@ def main():
         score = questions()
         results(score)
         retry = input("Do you want to take the test again? (y/n): ")
-        if retry.lower() != "y":
+        while retry.lower() not in ['y', 'n']:
+            retry = input("Invalid input. Please enter 'y' to retry or 'n' to exit: ")
+        if retry.lower() == 'n':
             break
         
 main()
