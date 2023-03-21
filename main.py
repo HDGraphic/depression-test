2275b2382441ba77fa19fb485ed20334efbdc195
Ivan Wong <127964300+HDGraphic@users.noreply.github.com>
2023-03-21 17:29:14 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-# Displaying the instructions
-print('-------------------------------------------------------------------------')
-print("              Welcome to the depression detection test")
-print('-------------------------------------------------------------------------')
-print('Please answer the following questions using a scale of 0 to 3, where:\n 0 = Never, 1 = Several days, 2 = half the days or more, 3 = Almost every day.\n')
-
-# Declaring the score variable
-score = 0
-      
-# Initialize a list to store the answers
-answers = []
+def introduction():
+      print('-------------------------------------------------------------------------')
+      print("              Welcome to the depression detection test")
+      print('-------------------------------------------------------------------------')
+      print('Please answer the following questions using a scale of 0 to 3, where:\n 0 = Never, 1 = Several days, 2 = half the days or more, 3 = Almost every day.\n')
